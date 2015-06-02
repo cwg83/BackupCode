ba3ffdcf629d9f00407d212027062da5d762e58d
cwg83 <cwg83@users.noreply.github.com>
2015-06-02 16:01:22 -0400

    Update ScriptVLookUp.js

--- ScriptVLookUp.js
+++ ScriptVLookUp.js
@@ -1,34 +1,39 @@
 function onFormSubmit(e) {
-   var date = e.values[0];
    var brand = e.values[1];
    var eGCs = e.values[2];
+//Format today's date as MM/DD/YYYY   
+   var today = new Date();
+   var dd = today.getDate();
+   var mm = today.getMonth()+1; //January is 0!
+
+   var yyyy = today.getFullYear();
+    if(dd<10){
+        dd='0'+dd
+    } 
+    if(mm<10){
+        mm='0'+mm
+    } 
+   var today = mm+'/'+dd+'/'+yyyy;
+   
+   
+   var ss = SpreadsheetApp.getActiveSpreadsheet();
+   var validation = ss.getSheetByName("Contacts");
+   var last = ss.getLastRow();
+   var data = validation.getRange(1,1,last,2).getValues();// create an array of data from columns A and B
+   for(nn=0;nn<data.length;++nn){
+    if (data[nn][0]==brand){break} ;// if a match in column B is found, break the loop
+      }
+   var contactEmail = data[nn][1];
+   
+   
 //Replace google docs line break format with html line breaks
    eGCs = eGCs.replace(/\n/g, '<br>'); 
-   
 //Send the email   
-   var subject = brand + "subject text" + date
+   var subject = brand + " eGCs to deactivate " + today
    var body    = "Hello " + brand + "," + "<br /><br />" 
-   + "Body text here";
+   + "The follow eGift Cards need to be deactivated:"  + "<br /><br />" 
+   + eGCs + "<br /><br />" 
+   + "Can you please deactivate them and report back the remaining balances?";
    var cc = "";
    MailApp.sendEmail(contactEmail, subject, body, {htmlBody: body, cc: cc});
    }
-   
-//Custom menu to run a script
-function onOpen() {
-  var ui = SpreadsheetApp.getUi();
-  // Or DocumentApp or FormApp.
-  ui.createMenu('Custom Menu')
-      .addItem('VLookUp', 'VLookUp')
-      .addToUi();
-}
-function VLookUp() {
-  var sh = SpreadsheetApp.getActiveSheet();
-  var ss = SpreadsheetApp.getActiveSpreadsheet();
-  var last=ss.getLastRow();
-  var data=sh.getRange(1,1,last,2).getValues();// create an array of data from columns A and B
-  var valA=Browser.inputBox('Enter value to search in A')
-  for(nn=0;nn<data.length;++nn){
-    if (data[nn][0]==valA){break} ;// if a match in column B is found, break the loop
-      }
-Browser.msgBox(data[nn][1]);// show column A
-}
