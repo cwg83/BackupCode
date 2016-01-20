65cb077b412b000d8ad76cd89b4483892ed6ff0b
cwg83 <cwg83@users.noreply.github.com>
2016-01-20 09:11:20 -0500

    Update CBParser.js

--- CBParser.js
+++ CBParser.js
@@ -1,201 +1,211 @@
-function SendToWorkLogCL() {
-
+  var user = Session.getActiveUser().getUserLoginId();
   var ss = SpreadsheetApp.getActiveSpreadsheet();
-  var sheet1 = ss.getSheetByName("CL");
   var sheet2 = ss.getSheetByName("Work Log");
   var repsheet = ss.getSheetByName("Representing");
-  var rep = sheet1.getRange("D15");
-  var repvalue = rep.getValue();
-  var order = sheet1.getRange("D17");
-  var ordervalue = order.getValue();
+  var updates = ss.getSheetByName("Updates");
   
-  var brand = sheet1.getRange("D16");
-  var brandvalue = brand.getValue();
+function SendToWorkLogCL() { //This is the script for the ClientLine sheet
+
+  var sheet1 = ss.getSheetByName("CL");
+  var rep = sheet1.getRange("B20");
+  var repvalue = rep.getValue();
+  var order = sheet1.getRange("B16");
+  var ordervalue = order.getValue();
+  var brand = sheet1.getRange("B11");
+  var purchase = sheet1.getRange("B15");
+  var stage = sheet1.getRange("B9");
+  var approver = sheet1.getRange("B18");
+  
+  SpreadsheetApp.getActiveSheet().getRange('B27').setValue(user);
   
   if (ordervalue.indexOf('CNZ') === -1) {
   Browser.msgBox("Please enter a valid Order #");
-  
-  }else if (repvalue == 'Yes') {
-  
+  return
+  }else if (brand.getValue() =="") {
+  Browser.msgBox("Please make sure the 'Brand' field is populated");
+  return
+  }else if (brand.getValue() =="#N/A") {
+  Browser.msgBox("This brand needs to be added to the Data Validation sheet");
+  return
+  }else if (purchase.getValue() =="") {
+  Browser.msgBox("Please make sure the 'Purchase Amount' field is populated");
+  return
+  }else if (approver.getValue() =="") {
+  Browser.msgBox("Please make sure the 'Approver' field is populated");
+  return
+  }
+  if (stage.getValue() == 'Graduated Chargeback' || stage.getValue() == 'Second Chargeback' ||  stage.getValue() == 'Pre-Arbitration') {
+  sheet1.getRange("A2:Y2").copyTo(updates.getRange(updates.getLastRow()+1,1,1,7), {contentsOnly:true});
+  }
+  if (repvalue == 'Yes') {
   sheet1.getRange("A2:Y2").copyTo(repsheet.getRange(repsheet.getLastRow()+1,1,1,7), {contentsOnly:true});
+  }
+  if (stage.getValue() == 'Retrieval' || stage.getValue() == 'Chargeback') {
   sheet1.getRange("A2:Y2").copyTo(sheet2.getRange(sheet2.getLastRow()+1,1,1,7), {contentsOnly:true});
-  SpreadsheetApp.getActiveSheet().getRange('A3:B39').setValue('');
-  SpreadsheetApp.getActiveSheet().getRange('A3').setValue('Ctrl+SHIFT+V');
-  SpreadsheetApp.getActiveSheet().getRange('D13').setValue('Yes');
-  SpreadsheetApp.getActiveSheet().getRange('D14:D15').setValue('No');
-  SpreadsheetApp.getActiveSheet().getRange('D17').setValue('');
-  SpreadsheetApp.getActiveSheet().getRange('D18').setValue('0');
-  SpreadsheetApp.getActiveSheet().getRange('D20').setValue('');
-  SpreadsheetApp.getActiveSheet().getRange('G12:G15').setValue('');
-  SpreadsheetApp.getActiveSheet().getRange('J12:J15').setValue('');
-  SpreadsheetApp.getActiveSheet().getRange('D16').setFormula("=IF(A2=\"\",\"\",VLOOKUP(E18,'Data Validation'!C2:E204,3,FALSE))");
-
-  }else{
-  
-  sheet1.getRange("A2:Y2").copyTo(sheet2.getRange(sheet2.getLastRow()+1,1,1,7), {contentsOnly:true});
-  SpreadsheetApp.getActiveSheet().getRange('A3:B39').setValue('');
-  SpreadsheetApp.getActiveSheet().getRange('A3').setValue('Ctrl+SHIFT+V');
-  SpreadsheetApp.getActiveSheet().getRange('D13').setValue('Yes');
-  SpreadsheetApp.getActiveSheet().getRange('D14:D15').setValue('No');
-  SpreadsheetApp.getActiveSheet().getRange('D17').setValue('');
-  SpreadsheetApp.getActiveSheet().getRange('D18').setValue('0');
-  SpreadsheetApp.getActiveSheet().getRange('D20').setValue('');
-  SpreadsheetApp.getActiveSheet().getRange('D16').setFormula("=IF(A2=\"\",\"\",VLOOKUP(E18,'Data Validation'!C2:E204,3,FALSE))");
-
- }
- }
+  }
+  SpreadsheetApp.getActiveSheet().getRange('B11').setFormula("=IF(A2=\"\",\"\",VLOOKUP($D$18,'Data Validation'!$C$2:$E,3,FALSE))"); //Brand
+  SpreadsheetApp.getActiveSheet().getRange('B9').setFormula("=IF($A$2=\"\",\"\",IF(RegExMatch($E$25,\"First Chargeback\"),\"Chargeback\",IF(RegExMatch($E$25,\"Second Chargeback\"),\"Second Chargeback\",IF(RegExMatch($E$25,\"Pre-Arbitration\"),\"Pre-Arbitration\",IF(RegExMatch($E$26,\"Retrieval\"),\"Retrieval\",\"\")))))"); //Stage Reached
+  SpreadsheetApp.getActiveSheet().getRange('B16').setValue(''); //Order #
+  SpreadsheetApp.getActiveSheet().getRange('B14').setFormula("=IF(A2=\"\",\"\",IF(REGEXMATCH($E$10,\"CAD\"),\"CAD\",\"USD\"))"); //Currency
+  SpreadsheetApp.getActiveSheet().getRange('B15').setFormula("=IF($B$14=\"USD\",TRIM($B$5),\"\")"); //Purchase Amount
+  SpreadsheetApp.getActiveSheet().getRange('B18').setValue(''); //Approver
+  SpreadsheetApp.getActiveSheet().getRange('B19:B20').setValue('No'); //Verified | Represented
+  SpreadsheetApp.getActiveSheet().getRange('B21:B26').setValue(''); //Rep Reason --> Documentation 4
+  SpreadsheetApp.getActiveSheet().getRange('B17').setValue('0'); //Balance
+  SpreadsheetApp.getActiveSheet().getRange('E3:F39').setValue(''); //Paste Section
+  SpreadsheetApp.getActiveSheet().getRange('E3').setValue('Ctrl+SHIFT+V'); //Paste Cell
+  }
  
 function SendToWorkLogAM() { //This is the script for the Amex sheet
 
-  var ss = SpreadsheetApp.getActiveSpreadsheet();
   var sheet1 = ss.getSheetByName("AM");
-  var sheet2 = ss.getSheetByName("Work Log");
-  var repsheet = ss.getSheetByName("Representing");
-  var rep = sheet1.getRange("D15");
+  var rep = sheet1.getRange("B20");
   var repvalue = rep.getValue();
-  var order = sheet1.getRange("D17");
+  var order = sheet1.getRange("B16");
   var ordervalue = order.getValue();
+  var brand = sheet1.getRange("B11");
+  var purchase = sheet1.getRange("B15");
+  var approver = sheet1.getRange("B18");
   
-  var brand = sheet1.getRange("D16");
-  var brandvalue = brand.getValue();
+  SpreadsheetApp.getActiveSheet().getRange('B27').setValue(user);
   
   if (ordervalue.indexOf('CNZ') === -1) {
   Browser.msgBox("Please enter a valid Order #");
+  return
+  }else if (brand.getValue() =="") {
+  Browser.msgBox("Please make sure the 'Brand' field is populated");
+  return
+  }else if (brand.getValue() =="#N/A") {
+  Browser.msgBox("This brand needs to be added to the Data Validation sheet");
+  return  
+  }else if (purchase.getValue() =="") {
+  Browser.msgBox("Please make sure the 'Purchase Amount' field is populated");
+  return
+  }else if (approver.getValue() =="") {
+  Browser.msgBox("Please make sure the 'Approver' field is populated");
+  return
+  }
   
-  }else if (repvalue == 'Yes') {
+  if (repvalue == 'Yes') {
   
   sheet1.getRange("A2:Y2").copyTo(repsheet.getRange(repsheet.getLastRow()+1,1,1,7), {contentsOnly:true});
-  sheet1.getRange("A2:Y2").copyTo(sheet2.getRange(sheet2.getLastRow()+1,1,1,7), {contentsOnly:true});
-  SpreadsheetApp.getActiveSheet().getRange('A3:B13').setValue('');
-  SpreadsheetApp.getActiveSheet().getRange('A3').setValue('Ctrl+SHIFT+V');
-  SpreadsheetApp.getActiveSheet().getRange('D13').setValue('Yes');
-  SpreadsheetApp.getActiveSheet().getRange('D14:D15').setValue('No');
-  SpreadsheetApp.getActiveSheet().getRange('D17').setValue('');
-  SpreadsheetApp.getActiveSheet().getRange('D18').setValue('0');
-  SpreadsheetApp.getActiveSheet().getRange('D20').setValue('');
-  SpreadsheetApp.getActiveSheet().getRange('G12:G15').setValue('');
-  SpreadsheetApp.getActiveSheet().getRange('D16').setFormula("=IF(A2=\"\",\"\",VLOOKUP(E18,'Data Validation'!D2:E204,2,FALSE))");
   
-  }else{
+  }
   
   sheet1.getRange("A2:Y2").copyTo(sheet2.getRange(sheet2.getLastRow()+1,1,1,7), {contentsOnly:true});
-  SpreadsheetApp.getActiveSheet().getRange('A3:B13').setValue('');
-  SpreadsheetApp.getActiveSheet().getRange('A3').setValue('Ctrl+SHIFT+V');
-  SpreadsheetApp.getActiveSheet().getRange('D13').setValue('Yes');
-  SpreadsheetApp.getActiveSheet().getRange('D14:D15').setValue('No');
-  SpreadsheetApp.getActiveSheet().getRange('D17').setValue('');
-  SpreadsheetApp.getActiveSheet().getRange('D18').setValue('0');
-  SpreadsheetApp.getActiveSheet().getRange('D20').setValue('');
-  SpreadsheetApp.getActiveSheet().getRange('G12:G15').setValue('');
-  SpreadsheetApp.getActiveSheet().getRange('D16').setFormula("=IF(A2=\"\",\"\",VLOOKUP(E18,'Data Validation'!D2:E204,2,FALSE))");
+  SpreadsheetApp.getActiveSheet().getRange('B11').setFormula("=IF(A2=\"\",\"\",VLOOKUP(D18,'Data Validation'!D2:E,2,FALSE))"); //Brand
+  SpreadsheetApp.getActiveSheet().getRange('B9').setFormula("=IF($A$2=\"\",\"\",IF($E$13=\"IQ\",\"Retrieval\",\"Chargeback\"))"); //Stage Reached
+  SpreadsheetApp.getActiveSheet().getRange('B16').setValue(''); //Order #
+  SpreadsheetApp.getActiveSheet().getRange('B14').setValue('USD'); //Currency
+  SpreadsheetApp.getActiveSheet().getRange('B18').setValue(''); //Approver
+  SpreadsheetApp.getActiveSheet().getRange('B19:B20').setValue('No'); //Verification | Represented
+  SpreadsheetApp.getActiveSheet().getRange('B21:B26').setValue(''); //Rep Reason --> Documentation 4
+  SpreadsheetApp.getActiveSheet().getRange('B17').setValue('0'); //Balance
+  SpreadsheetApp.getActiveSheet().getRange('E3:F13').setValue(''); //Paste area
+  SpreadsheetApp.getActiveSheet().getRange('E3').setValue('Ctrl+SHIFT+V'); //Paste cell
 
- }
- }
+  }
  
 function SendToWorkLogPP() { //This is the script for the PayPal sheet
 
-  var ss = SpreadsheetApp.getActiveSpreadsheet();
   var sheet1 = ss.getSheetByName("PP");
-  var sheet2 = ss.getSheetByName("Work Log");
-  var repsheet = ss.getSheetByName("Representing");
-  var rep = sheet1.getRange("D15");
+  var rep = sheet1.getRange("B20");
   var repvalue = rep.getValue();
-  var order = sheet1.getRange("D17");
+  var order = sheet1.getRange("B16");
   var ordervalue = order.getValue();
+  var brand = sheet1.getRange("B15");
+  var purchase = sheet1.getRange("B14");
+  var approver = sheet1.getRange("B18");  
   
-  var brand = sheet1.getRange("D16");
-  var brandvalue = brand.getValue();
+  SpreadsheetApp.getActiveSheet().getRange('B27').setValue(user);
   
   if (ordervalue.indexOf('CNZ') === -1) {
   Browser.msgBox("Please enter a valid Order #");
+  return
+  }else if (brand.getValue() =="") {
+  Browser.msgBox("Please make sure the 'Brand' field is populated");
+  return
+  }else if (brand.getValue() =="#N/A") {
+  Browser.msgBox("This brand needs to be added to the Data Validation sheet");
+  return  
+  }else if (purchase.getValue() =="") {
+  Browser.msgBox("Please make sure the 'Purchase Amount' field is populated");
+  return
+  }else if (approver.getValue() =="") {
+  Browser.msgBox("Please make sure the 'Approver' field is populated");
+  return
+  }
   
-  }else if (repvalue == 'Yes') {
+  if (repvalue == 'Yes') {
   
   sheet1.getRange("A2:Y2").copyTo(repsheet.getRange(repsheet.getLastRow()+1,1,1,7), {contentsOnly:true});
-  sheet1.getRange("A2:Y2").copyTo(sheet2.getRange(sheet2.getLastRow()+1,1,1,7), {contentsOnly:true});
-  SpreadsheetApp.getActiveSheet().getRange('A3:A6').setValue('');
-  SpreadsheetApp.getActiveSheet().getRange('A11:A23').setValue('');
-  SpreadsheetApp.getActiveSheet().getRange('B3:B23').setValue('');
-  SpreadsheetApp.getActiveSheet().getRange('A3').setValue('Ctrl+SHIFT+V');
-  SpreadsheetApp.getActiveSheet().getRange('D13').setValue('Yes');
-  SpreadsheetApp.getActiveSheet().getRange('D14:D15').setValue('No');
-  SpreadsheetApp.getActiveSheet().getRange('D17').setValue('');
-  SpreadsheetApp.getActiveSheet().getRange('D18').setValue('0');
-  SpreadsheetApp.getActiveSheet().getRange('D20').setValue('');
-  SpreadsheetApp.getActiveSheet().getRange('G12:G15').setValue('');
-  SpreadsheetApp.getActiveSheet().getRange('A7').setValue('Amount:');
-  SpreadsheetApp.getActiveSheet().getRange('A8').setValue('Trans Date:');
-  SpreadsheetApp.getActiveSheet().getRange('A9').setValue('Case #:');
-  SpreadsheetApp.getActiveSheet().getRange('A10').setValue('Reason Code:');
-  SpreadsheetApp.getActiveSheet().getRange('A11').setValue('Dispute Date:');
   
-  }else{
+  }
   
   sheet1.getRange("A2:Y2").copyTo(sheet2.getRange(sheet2.getLastRow()+1,1,1,7), {contentsOnly:true});
-  SpreadsheetApp.getActiveSheet().getRange('A3:A6').setValue('');
-  SpreadsheetApp.getActiveSheet().getRange('A11:A23').setValue('');
-  SpreadsheetApp.getActiveSheet().getRange('B3:B23').setValue('');
-  SpreadsheetApp.getActiveSheet().getRange('A3').setValue('Ctrl+SHIFT+V');
-  SpreadsheetApp.getActiveSheet().getRange('D13').setValue('Yes');
-  SpreadsheetApp.getActiveSheet().getRange('D14:D15').setValue('No');
-  SpreadsheetApp.getActiveSheet().getRange('D16').setValue('');
-  SpreadsheetApp.getActiveSheet().getRange('D17').setValue('');
-  SpreadsheetApp.getActiveSheet().getRange('D18').setValue('0');
-  SpreadsheetApp.getActiveSheet().getRange('D20').setValue('');
-  SpreadsheetApp.getActiveSheet().getRange('A7').setValue('Amount:');
-  SpreadsheetApp.getActiveSheet().getRange('A8').setValue('Trans Date:');
-  SpreadsheetApp.getActiveSheet().getRange('A9').setValue('Case #:');
-  SpreadsheetApp.getActiveSheet().getRange('A10').setValue('Reason Code:');
-  SpreadsheetApp.getActiveSheet().getRange('A11').setValue('Dispute Date:');
+  SpreadsheetApp.getActiveSheet().getRange('B15').setValue(''); //Brand
+  SpreadsheetApp.getActiveSheet().getRange('B16').setValue(''); //Order #
+  SpreadsheetApp.getActiveSheet().getRange('B13').setValue('USD'); //Currency
+  SpreadsheetApp.getActiveSheet().getRange('B18').setValue(''); //Approver
+  SpreadsheetApp.getActiveSheet().getRange('B19:B20').setValue('No'); //Verification | Represented
+  SpreadsheetApp.getActiveSheet().getRange('B21:B26').setValue('');  //Rep Reason --> Documentation 4
+  SpreadsheetApp.getActiveSheet().getRange('B17').setValue('0'); //Balance
+  SpreadsheetApp.getActiveSheet().getRange('E3:F14').setValue(''); // Paste Area
+  SpreadsheetApp.getActiveSheet().getRange('E3').setValue('Ctrl+SHIFT+V'); //Paste Cell
+  SpreadsheetApp.getActiveSheet().getRange('E7').setValue('Amount:');
+  SpreadsheetApp.getActiveSheet().getRange('E8').setValue('Trans Date:');
+  SpreadsheetApp.getActiveSheet().getRange('E9').setValue('Case #:');
+  SpreadsheetApp.getActiveSheet().getRange('E10').setValue('Reason Code:');
+  SpreadsheetApp.getActiveSheet().getRange('E11').setValue('Dispute Date:');
 
- }
- }
+  
+  }
  
 function SendToWorkLogAD() { //This is the script for the Adyen sheet
 
-  var ss = SpreadsheetApp.getActiveSpreadsheet();
   var sheet1 = ss.getSheetByName("AD");
-  var sheet2 = ss.getSheetByName("Work Log");
-  var repsheet = ss.getSheetByName("Representing");
-  var rep = sheet1.getRange("B21");
+  var rep = sheet1.getRange("B20");
   var repvalue = rep.getValue();
-  var order = sheet1.getRange("B23");
+  var order = sheet1.getRange("B16");
   var ordervalue = order.getValue();
+  var brand = sheet1.getRange("B15");
+  var purchase = sheet1.getRange("B14");
+  var approver = sheet1.getRange("B18");  
   
-  var brand = sheet1.getRange("B22");
-  var brandvalue = brand.getValue();
+  SpreadsheetApp.getActiveSheet().getRange('B27').setValue(user);
   
   if (ordervalue.indexOf('CNZ') === -1) {
   Browser.msgBox("Please enter a valid Order #");
+  return
+  }else if (brand.getValue() =="") {
+  Browser.msgBox("Please make sure the 'Brand' field is populated");
+  return
+  }else if (brand.getValue() =="#N/A") {
+  Browser.msgBox("This brand needs to be added to the Data Validation sheet");
+  return
+  }else if (purchase.getValue() =="") {
+  Browser.msgBox("Please make sure the 'Purchase Amount' field is populated");
+  return
+  }else if (approver.getValue() =="") {
+  Browser.msgBox("Please make sure the 'Approver' field is populated");
+  return
+  }
   
-  }else if (repvalue == 'Yes') {
+  if (repvalue == 'Yes') {
   
   sheet1.getRange("A2:Y2").copyTo(repsheet.getRange(repsheet.getLastRow()+1,1,1,7), {contentsOnly:true});
-  sheet1.getRange("A2:Y2").copyTo(sheet2.getRange(sheet2.getLastRow()+1,1,1,7), {contentsOnly:true});
-  SpreadsheetApp.getActiveSheet().getRange('A3:F8').setValue('');
-  SpreadsheetApp.getActiveSheet().getRange('A3').setValue('Ctrl+SHIFT+V');
-  SpreadsheetApp.getActiveSheet().getRange('B18').setValue('Chargeback');
-  SpreadsheetApp.getActiveSheet().getRange('B19').setValue('Yes');
-  SpreadsheetApp.getActiveSheet().getRange('B20:B21').setValue('No');
-  SpreadsheetApp.getActiveSheet().getRange('B22').setValue('');
-  SpreadsheetApp.getActiveSheet().getRange('B23').setValue('');
-  SpreadsheetApp.getActiveSheet().getRange('B24').setValue('0');
-  SpreadsheetApp.getActiveSheet().getRange('B26').setValue('');
-  SpreadsheetApp.getActiveSheet().getRange('E18:E21').setValue('');
-  SpreadsheetApp.getActiveSheet().getRange('H18:H21').setValue('');
   
-  }else{
+  }
   
   sheet1.getRange("A2:Y2").copyTo(sheet2.getRange(sheet2.getLastRow()+1,1,1,7), {contentsOnly:true});
-  SpreadsheetApp.getActiveSheet().getRange('A3:F8').setValue('');
-  SpreadsheetApp.getActiveSheet().getRange('A3').setValue('Ctrl+SHIFT+V');
-  SpreadsheetApp.getActiveSheet().getRange('B18').setValue('Chargeback');
-  SpreadsheetApp.getActiveSheet().getRange('B19').setValue('Yes');
-  SpreadsheetApp.getActiveSheet().getRange('B20:B21').setValue('No');
-  SpreadsheetApp.getActiveSheet().getRange('B22').setValue('');
-  SpreadsheetApp.getActiveSheet().getRange('B23').setValue('');
-  SpreadsheetApp.getActiveSheet().getRange('B24').setValue('0');
-  SpreadsheetApp.getActiveSheet().getRange('B26').setValue('');
+  SpreadsheetApp.getActiveSheet().getRange('B15').setValue(''); //Brand
+  SpreadsheetApp.getActiveSheet().getRange('B9').setValue('Chargeback'); //Stage Reached
+  SpreadsheetApp.getActiveSheet().getRange('B16').setValue(''); //Order #
+  SpreadsheetApp.getActiveSheet().getRange('B18').setValue(''); //Approver
+  SpreadsheetApp.getActiveSheet().getRange('B19:B20').setValue('No'); //Verification | Represented
+  SpreadsheetApp.getActiveSheet().getRange('B21:B26').setValue(''); //Rep Reason --> Documentation 4
+  SpreadsheetApp.getActiveSheet().getRange('B17').setValue('0'); //Balance
+  SpreadsheetApp.getActiveSheet().getRange('E3:I8').setValue(''); //Paste area
+  SpreadsheetApp.getActiveSheet().getRange('E3').setValue('Ctrl+SHIFT+V'); //Paste cell
   
- }
  }
